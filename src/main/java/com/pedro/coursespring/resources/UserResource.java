8c45cb8e24f20437c6128433253daf34765b96dc
PedrinL27 <pedrolangelineto0@gmail.com>
2026-05-07 14:32:16 -0300

    User insert

--- src/main/java/com/pedro/coursespring/resources/UserResource.java
+++ src/main/java/com/pedro/coursespring/resources/UserResource.java
@@ -1,10 +1,12 @@
 package com.pedro.coursespring.resources;
 
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.pedro.coursespring.entities.User;
 import com.pedro.coursespring.services.UserService;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +14,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 @RestController
 @RequestMapping(value = "/users")
@@ -31,6 +36,19 @@ public class UserResource {
         User obj = service.findById(id);
         return ResponseEntity.ok().body(obj); 
     }
+
+    @PostMapping
+    public ResponseEntity<User> insert(@RequestBody User obj) {
+        obj = service.insert(obj);
+        URI uri = ServletUriComponentsBuilder
+        .fromCurrentRequest()
+        .path("/{id}")
+        .buildAndExpand(obj.getId())
+        .toUri();
+
+        return ResponseEntity.created(uri).body(obj);
+    }
+    
 }
     
 
