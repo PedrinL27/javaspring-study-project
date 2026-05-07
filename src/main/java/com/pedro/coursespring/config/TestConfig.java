7c6d54f141fbf6a81c0f9c69bd498f3d3b01f8db
PedrinL27 <pedrolangelineto0@gmail.com>
2026-05-07 14:05:59 -0300

    Added the payment entity

--- src/main/java/com/pedro/coursespring/config/TestConfig.java
+++ src/main/java/com/pedro/coursespring/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.pedro.coursespring.entities.Category;
 import com.pedro.coursespring.entities.Order;
 import com.pedro.coursespring.entities.OrderItem;
+import com.pedro.coursespring.entities.Payment;
 import com.pedro.coursespring.entities.Product;
 import com.pedro.coursespring.entities.User;
 import com.pedro.coursespring.entities.enums.OrderStatus;
@@ -80,6 +81,11 @@ public class TestConfig implements CommandLineRunner {
         OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 
         orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+
+        Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+
+        o1.setPayment(pay1);
+        orderRepository.save(o1);
     }
 
 
